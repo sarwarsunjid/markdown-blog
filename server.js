c9eb9a98b68653d51e572b705e5f2fdb7d202697
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-08-18 15:48:24 +0600

    Created router articles.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/articles', articleRouter)
+
 app.get('/', (req, res) =>{
     //res.send('Hello World')
-    res.render('index')
+    //res.render('index', { articles:articles })
+    const articles = [{
+        title : 'Test Articles',
+        description : 'Test description',
+        createdAt : Date.now(),
+    }]
+    res.render('index', { articles: articles })
 })
 
 app.listen(5000)
